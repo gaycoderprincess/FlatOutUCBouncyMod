1fdd0737f2a1d1c38fc3c933fabcd9eb28cb2c91
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-01-09 23:58:39 +0100

    car-to-car only test

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ void ValueEditorMenu(float& value) {
 
 bool bBouncyEnabled = true;
 bool bBouncyEnabledForPlayer = false;
+bool bBouncyEnabledForCarsOnly = false;
 float fBouncyScale = 2.5;
 float fCurrentBouncyScale = 1.0;
 
@@ -44,14 +45,29 @@ void MenuLoop() {
 		bBouncyEnabledForPlayer = !bBouncyEnabledForPlayer;
 	}
 
+	if (DrawMenuOption(std::format("Car-to-Car Only - {}", bBouncyEnabledForCarsOnly))) {
+		bBouncyEnabledForCarsOnly = !bBouncyEnabledForCarsOnly;
+	}
+
 	ChloeMenuLib::EndMenu();
 }
 
-void __fastcall BouncyCarCheck(Car* pCar, void* origPtr) {
+bool IsObjectAPlayer(Car* pCar) {
+	for (int i = 0; i < 32; i++) {
+		auto ply = GetPlayer(i);
+		if (!ply) continue;
+		if (pCar == ply->pCar) return true;
+	}
+	return false;
+}
+
+void __fastcall BouncyCarCheck(Car* pCar, void* origPtr, Car* pTarget) {
+	fCurrentBouncyScale = 1.0;
 	if (!origPtr) return;
 
 	fCurrentBouncyScale = fBouncyScale;
 	if (!bBouncyEnabledForPlayer && pCar == GetPlayer(0)->pCar) fCurrentBouncyScale = 1.0;
+	if (bBouncyEnabledForCarsOnly && (!IsObjectAPlayer(pCar) || !IsObjectAPlayer(pTarget))) fCurrentBouncyScale = 1.0;
 }
 
 // skip writing to the damage value for remote players
@@ -59,6 +75,12 @@ uintptr_t BouncyCarCheckASM1_jmp = 0x5D631E;
 float __attribute__((naked)) BouncyCarCheckASM1() {
 	__asm__ (
 		"pushad\n\t"
+
+		// target
+		"mov eax, [ecx+4]\n\t"
+		"sub eax, 0x1C0\n\t"
+		"push eax\n\t"
+
 		"mov eax, [ebx+0x10]\n\t"
 		"mov ecx, [eax]\n\t"
 		"sub ecx, 0x1C0\n\t"
@@ -78,6 +100,13 @@ float __attribute__((naked)) BouncyCarCheckASM2() {
 	__asm__ (
 		"mov eax, [ecx+4]\n\t"
 		"pushad\n\t"
+
+		// previous target
+		"mov ebx, [ebx+0x10]\n\t"
+		"mov ecx, [ebx]\n\t"
+		"sub ecx, 0x1C0\n\t"
+		"push ecx\n\t"
+
 		"mov ecx, eax\n\t"
 		"sub ecx, 0x1C0\n\t"
 		"mov edx, eax\n\t"
